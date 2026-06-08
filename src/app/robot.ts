33b68cb3d9ff0840bbaa3c7f643c58a14126f15b
teomax5 <73957210+teomax5@users.noreply.github.com>
2026-06-08 12:41:40 +0530

    robot error fixing

--- src/app/robot.ts
+++ src/app/robot.ts
@@ -6,6 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://repurpose-ai-landing.vercel.app/sitemap.xml",
+    sitemap:
+      "https://repurpose-ai-landing.vercel.app/sitemap.xml",
   };
 }
