99197e37abcd11835ab087d2b98d34d6ecd3496f
teomax5 <73957210+teomax5@users.noreply.github.com>
2026-06-08 12:50:03 +0530

    robots fix

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,8 @@
-import { MetadataRoute } from "next";
-
-export default function robots(): MetadataRoute.Robots {
+export default function robots() {
   return {
     rules: {
       userAgent: "*",
       allow: "/",
     },
-    sitemap:
-      "https://repurpose-ai-landing.vercel.app/sitemap.xml",
   };
 }
